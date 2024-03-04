7dae07d0f5bd6c9cedc83d22979a4e905f27323c
Schegol4ik <iliyinchikartem@gmail.com>
2024-03-04 16:30:03 +0100

    hw4

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,13 @@ import { HW4 } from './HW/4/HW4';
 
 function App() {
   return (
-    <div className="App">
-      <HW1 />
-      <HW2 />
-      <HW3 />
-      {/*<HW4 />*/}
-    </div>
+      <div className="App">
+
+          <HW1/>
+          <HW2/>
+          <HW3/>
+          <HW4 />
+      </div>
   );
 }
 
